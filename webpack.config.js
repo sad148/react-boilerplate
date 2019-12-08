bffa330272262ddd87f053d1f2a57fc2226b5022
Saurabh Dhamnaskar <sad148@pitt.edu>
2019-12-08 16:05:38 -0500

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var path = require('path');
+const devMode = process.env.NODE_ENV !== 'production';
 module.exports = {
+  optimization: {
+    minimizer: [new UglifyJsPlugin({
+      uglifyOptions: {
+        // Eliminate comments
+        comments: false,
+        compress: {
+          // Drop console statements
+          drop_console: true
+        }
+      }
+    })],
+    removeAvailableModules: true,
+    sideEffects: true
+  },
   entry: './src/index.js',
   output: {
-    path : __dirname + '/dist',
-    filename : 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
-  optimization: {
-    minimizer: [new UglifyJsPlugin()],
+  devServer: {
+    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -28,15 +45,30 @@ module.exports = {
         ]
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      }
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
     })
   ]
 };
